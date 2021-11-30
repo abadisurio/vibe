876c091530ab7afb92f4a24e6edacaa685c89e50
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-11-30 17:54:12 +0700

    ViBE: add asset logo and font

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff073FCF),
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 40,
+                color: Color(0xff272727)),
+            headline2: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 35,
+                color: Color(0xff868686)),
+            headline3: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30,
+            ),
+            headline4: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: Color(0xFF000000)),
+            headline5: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+            headline6: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 15,
+            ),
+            bodyText1: TextStyle(
+                fontWeight: FontWeight.w600, color: Color(0xff272727)),
+            // bodyText2: TextStyle(
+            //     fontWeight: FontWeight.w400,
+            //     fontSize: 15,
+            //     color: Color(0xff868686)),
+            button: TextStyle(fontWeight: FontWeight.w700)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
