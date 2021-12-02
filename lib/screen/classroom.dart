f9c831f3487a5a8dbfdb980ecee55fe0ddbf1484
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-12-02 11:31:07 +0700

    classroom: layouting and add calendar

--- lib/screen/classroom.dart
+++ lib/screen/classroom.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:vibe/component/flat_card.dart';
 
 class ClassRoom extends StatefulWidget {
   const ClassRoom({Key? key}) : super(key: key);
@@ -10,6 +12,46 @@ class ClassRoom extends StatefulWidget {
 class _ClassRoomState extends State<ClassRoom> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Classroom",
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        // leading: IconButton(
+        //     onPressed: () {},
+        //     icon: const Icon(Icons.arrow_back),
+        //     color: Colors.black),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.ac_unit))
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                FlatCard(),
+                FlatCard(),
+                FlatCard(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
